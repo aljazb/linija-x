8657d4f5d9a80241d11cfb21f2240defdb498280
markozeman <mz4730@student.uni-lj.si>
2016-12-03 14:37:30 +0000

    Added bootstrap and JS events

--- js/script.js
+++ js/script.js
@@ -2,5 +2,33 @@ $(document).ready(main);
 
 
 function main() {
+    $("#uporabnik").click(uporabnik_clicked);
+    $("#prevoznik").click(prevoznik_clicked);
+    $("#potrditev_prevoznika").click(potrditev_prevoznika);
+    
+    
+}
+
+
+
+function uporabnik_clicked() {
+    $(".prevoznik").hide();
+    $(".uporabnik").show(500);
+    
+    $("#prevoznik").removeClass("active");
+    $("#uporabnik").addClass("active");
+}
+
+
+function prevoznik_clicked() {
+    $(".uporabnik").hide();
+    $(".prevoznik").show(500);
+    
+    $("#uporabnik").removeClass("active");
+    $("#prevoznik").addClass("active");
+}
+
+
+function potrditev_prevoznika() {
     
 }
